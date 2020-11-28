e8f3b1585a3df65ff1ad3a76b5c176564ce7b98e
Jordi Lazo <jordilazo94@gmail.com>
2020-11-28 15:13:33 +0100

    update

--- ghostcell.py
+++ ghostcell.py
@@ -45,7 +45,18 @@ class Bot:
                 arg_3 = int(inputs[4])
                 arg_4 = int(inputs[5])
                 arg_5 = int(inputs[6])
+                
+                if entity_type == "FACTORY":
+                    self.factories[entity_id]=Factory(entity_id,entity_type,arg_1,arg_2,arg_3)
+                if entity_type == "TROOP":
+                    self.factories[entity_id] = Troop(entity_id, entity_type, arg_1, arg_2, arg_3,arg_4,arg_5)
+                
+                
+                prnt(self.factories)
+                prnt(self.factories)
+                prnt(inputs)
             #action()
+
 
     def factory_puntuation(self, factory):
         return 0
@@ -61,4 +72,3 @@ for i in range(link_count):
     factory_1, factory_2, distance = [int(j) for j in input().split()]
     distances.setdefault(factory_1,{factory_2: distance})
     distances[factory_1][factory_2] = distance
-prnt(distances)
